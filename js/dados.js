f1793f67ae682c62f48d3b5c2e706c751c4c8522
William Ferreira <119333308+williamferreiradev@users.noreply.github.com>
2023-08-20 17:49:41 -0300

    Initial commit

---
+++ js/dados.js
@@ -0,0 +1,473 @@
+var MENU = {
+    "burgers": [
+        {
+            "id": "corrente1",
+            "img": "./img/cardapio/correntes/corrente (1).jpg",
+            "name": "Força de Prata",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 259
+        },
+
+        {
+            "id": "corrente2",
+            "img": "./img/cardapio/correntes/corrente (2).jpg",
+            "name": "Corrente Poderosa",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 359
+        },
+
+        {
+            "id": "corrente3",
+            "img": "./img/cardapio/correntes/corrente (3).jpg",
+            "name": "Elos Robustos",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "corrente4",
+            "img": "./img/cardapio/correntes/corrente (4).jpg",
+            "name": "Guardião Prateado",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "corrente5",
+            "img": "./img/cardapio/correntes/corrente (5).jpg",
+            "name": "Espírito Livre",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 89
+        },
+
+        {
+            "id": "corrente6",
+            "img": "./img/cardapio/correntes/corrente (6).jpg",
+            "name": "Vida Prateada",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "corrente7",
+            "img": "./img/cardapio/correntes/corrente (7).jpg",
+            "name": "Aço Fino",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 359
+        },
+
+        {
+            "id": "corrente8",
+            "img": "./img/cardapio/correntes/corrente (8).jpg",
+            "name": "Corrente Valorosa",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "corrente9",
+            "img": "./img/cardapio/correntes/corrente (9).jpg",
+            "name": "Elegância Viril",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "corrente10",
+            "img": "./img/cardapio/correntes/corrente (10).jpg",
+            "name": "Estilo Masculino",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+    ],
+    
+    "pizzas": [
+        {
+            "id": "anel1",
+            "img": "./img/cardapio/aneis/anel (1).jpg",
+            "name": "Prata Encantada",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 359
+        },
+
+        {
+            "id": "anel2",
+            "img": "./img/cardapio/aneis/anel (2).jpg",
+            "name": "Prateada Radiante",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 259
+        },
+
+        {
+            "id": "anel3",
+            "img": "./img/cardapio/aneis/anel (3).jpg",
+            "name": "Prata Estelar",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 99
+        },
+
+        {
+            "id": "anel4",
+            "img": "./img/cardapio/aneis/anel (4).jpg",
+            "name": "Prata do Crepúsculo",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 859
+        },
+
+        {
+            "id": "anel5",
+            "img": "./img/cardapio/aneis/anel (5).jpg",
+            "name": "Prata Sereno",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "anel6",
+            "img": "./img/cardapio/aneis/anel (6).jpg",
+            "name": "Estrela Prateada",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 359
+        },
+
+        {
+            "id": "anel7",
+            "img": "./img/cardapio/aneis/anel (7).jpg",
+            "name": "Safira Lua",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "anel8",
+            "img": "./img/cardapio/aneis/anel (8).jpg",
+            "name": "Rubi Frio",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "anel9",
+            "img": "./img/cardapio/aneis/anel (9).jpg",
+            "name": "Esmeralda Brilho",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "anel10",
+            "img": "./img/cardapio/aneis/anel (10).jpg",
+            "name": "Alicerce Prateado",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+    ],
+
+    "churrasco": [
+        {
+            "id": "tornozeleira1",
+            "img": "./img/cardapio/tornozeleiras/tornozeleira (1).jpg",
+            "name": "Tornozeleira Celeste",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "tornozeleira2",
+            "img": "./img/cardapio/tornozeleiras/tornozeleira (2).jpg",
+            "name": "Brisa Lunar",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 259
+        },
+
+       
+        {
+            "id": "tornozeleira3",
+            "img": "./img/cardapio/tornozeleiras/tornozeleira (3).jpg",
+            "name": "Elegância de Prata",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 89
+        },
+
+        {
+            "id": "tornozeleira4",
+            "img": "./img/cardapio/tornozeleiras/tornozeleira (4).jpg",
+            "name": "Pérola Prateada",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "tornozeleira5",
+            "img": "./img/cardapio/tornozeleiras/tornozeleira (5).jpg",
+            "name": "Encanto Noturno",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "tornozeleira6",
+            "img": "./img/cardapio/tornozeleiras/tornozeleira (6).jpg",
+            "name": "Estrela Prateada",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "tornozeleira7",
+            "img": "./img/cardapio/tornozeleiras/tornozeleira (7).jpg",
+            "name": "Trança Argêntea",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 259
+        },
+
+        {
+            "id": "tornozeleira8",
+            "img": "./img/cardapio/tornozeleiras/tornozeleira (8).jpg",
+            "name": "Serenidade",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "tornozeleira9",
+            "img": "./img/cardapio/tornozeleiras/tornozeleira (9).jpg",
+            "name": "Toque Delicado",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 459
+        },
+
+        {
+            "id": "tornozeleira10",
+            "img": "./img/cardapio/tornozeleiras/tornozeleira (10).jpg",
+            "name": "Tornozeleira Cintilante",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 359
+        },
+
+    ],
+
+    "steaks": [
+        {
+            "id": "pul1",
+            "img": "./img/cardapio/pulseiras/pulseira (1).jpg",
+            "name": "Lua Prateada",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 359
+        },
+
+        {
+            "id": "pul2",
+            "img": "./img/cardapio/pulseiras/pulseira (2).jpg",
+            "name": "Pulseira Delicada",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 559
+        },
+
+        {
+            "id": "pul3",
+            "img": "./img/cardapio/pulseiras/pulseira (3).jpg",
+            "name": "Charme de Prata",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "pul4",
+            "img": "./img/cardapio/pulseiras/pulseira (4).jpg",
+            "name": "Pulseira Estelar",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 38
+        },
+
+        {
+            "id": "pul5",
+            "img": "./img/cardapio/pulseiras/pulseira (5).jpg",
+            "name": "Pérola Cintilante",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "pul6",
+            "img": "./img/cardapio/pulseiras/pulseira (6).jpg",
+            "name": "Estilo Prateado",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 359
+        },
+
+        {
+            "id": "pul7",
+            "img": "./img/cardapio/pulseiras/pulseira (7).jpg",
+            "name": "Elegância Trançada",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 259
+        },
+
+        {
+            "id": "pul8",
+            "img": "./img/cardapio/pulseiras/pulseira (8).jpg",
+            "name": "Brilho Suti",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "pul9",
+            "img": "./img/cardapio/pulseiras/pulseira (9).jpg",
+            "name": "Laço Prateado",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 109
+        },
+    ],
+
+    "bebidas": [
+        {
+            "id": "gravata",
+            "img": "./img/cardapio/gravatinha/gravatinha (1).jpg",
+            "name": "Gravata Prateada",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "gravata2",
+            "img": "./img/cardapio/gravatinha/gravatinha (2).jpg",
+            "name": "Charme Argênteo",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 159
+        },
+
+        {
+            "id": "gravata3",
+            "img": "./img/cardapio/gravatinha/gravatinha (3).jpg",
+            "name": "Elegância Sofisticada",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price":365
+        },
+
+        {
+            "id": "gravata4",
+            "img": "./img/cardapio/gravatinha/gravatinha (4).jpg",
+            "name": "Nó Prata Fino",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 502
+        },
+
+        {
+            "id": "gravata5",
+            "img": "./img/cardapio/gravatinha/gravatinha (5).jpg",
+            "name": "Laço Radiante",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 180
+        },
+
+        {
+            "id": "gravata6",
+            "img": "./img/cardapio/gravatinha/gravatinha (6).jpg",
+            "name": "Gravata Brilhante",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 105
+        },
+
+        {
+            "id": "gravata7",
+            "img": "./img/cardapio/gravatinha/gravatinha (7).jpg",
+            "name": "Estilo Clássico",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 259
+        },
+
+        {
+            "id": "gravata8",
+            "img": "./img/cardapio/gravatinha/gravatinha (8).jpg",
+            "name": "Nó Elegante",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 359
+        },
+       
+    ],
+
+    "sobremesas": [
+        {
+            "id": "colar",
+            "img": "./img/cardapio/colares/colar (1).jpg",
+            "name": "Coração Prateado",
+            "desc": "Colar 123",
+            "price": 250.00
+        },
+
+        {
+            "id": "colar2",
+            "img": "./img/cardapio/colares/colar (2).jpg",
+            "name": "Rosa Celeste",
+            "desc": "Colar 123",
+            "price": 150.00
+        },
+
+        {
+            "id": "colar3",
+            "img": "./img/cardapio/colares/colar (3).jpg",
+            "name": "Elegância D'Prata",
+            "desc": "Colar 123",
+            "price": 120.00
+        },
+
+        {
+            "id": "colar4",
+            "img": "./img/cardapio/colares/colar (4).jpg",
+            "name": "Pérola Cintilante",
+            "desc": "Colar 123",
+            "price": 80.00
+        },
+
+        {
+            "id": "colar5",
+            "img": "./img/cardapio/colares/colar (5).jpg",
+            "name": "Encanto de Prata",
+            "desc": "Colar 123",
+            "price": 350.00
+        },
+
+        {
+            "id": "colar6",
+            "img": "./img/cardapio/colares/colar (6).jpg",
+            "name": "Estrela Noturna",
+            "desc": "Colar 123",
+            "price": 190.00
+        },
+
+        {
+            "id": "colar7",
+            "img": "./img/cardapio/colares/colar (7).jpg",
+            "name": "Brisa Lunar",
+            "desc": "Colar 123",
+            "price": 150.00
+        },
+
+        {
+            "id": "colar8",
+            "img": "./img/cardapio/colares/colar (8).jpg",
+            "name": "Lua Prateada",
+            "desc": "Colar 123",
+            "price": 450.00
+        },
+
+        {
+            "id": "colar9",
+            "img": "./img/cardapio/colares/colar (9).jpg",
+            "name": "Fada Argêntea",
+            "desc": "Colar 123",
+            "price": 170.00
+        },
+
+        {
+            "id": "colar10",
+            "img": "./img/cardapio/colares/colar (10).jpg",
+            "name": "Serenidade Prata",
+            "desc": "Colar 123",
+            "price": 80.00
+        },
+       
+    ]
+}
